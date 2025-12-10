1264e2e26a5c6f80dd04903d6ea9527fb65e85fd
leveragewithAI <lungurobert990@gmail.com>
2025-12-10 10:09:27 -0500

    feat(Ingestion Pipeline): Construct DB

--- src/ingestion/pipeline.py
+++ src/ingestion/pipeline.py
@@ -2,8 +2,20 @@ import os
 from loaders.load_generic import load_document
 from chunkers.text_chunker import chunk_documents
 from embeddings.embedder import embed_chunks
+from db.construct import insert_into_pinecone
 from pathlib import Path
 import nltk
+from pinecone import Pinecone, ServerlessSpec
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+index_name = "rag-index"
+
+
+index = pc.Index(index_name)
 
 resources = [
     "punkt",
@@ -30,11 +42,7 @@ def ingest_folder(folder: str):
 
         embedded_chunks = embed_chunks(chunks)
 
-        print(embedded_chunks)
-
-
-
-    
+        insert_into_pinecone(embedded_chunks, index)
 
 if __name__ == "__main__":
     base_path = Path(__file__).resolve().parents[3] / "Documents"
